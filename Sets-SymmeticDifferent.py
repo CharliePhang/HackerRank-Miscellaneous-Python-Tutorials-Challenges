c5fb5e7af6616b2cd329e4733d58bf49deb31963
chalypeng <chalypeng@gmail.com>
2015-01-30 10:29:05 +0800

    3Commit

---
+++ Sets-SymmeticDifferent.py
@@ -0,0 +1,14 @@
+M = int(raw_input())
+firstSet = set(list(map(int, raw_input().split())))
+
+N = int(raw_input())
+secondSet = set(list(map(int, raw_input().split())))
+
+SymmetricDiffSet = firstSet.union(secondSet).difference(
+    firstSet.intersection(secondSet))
+
+result = list(SymmetricDiffSet)
+result.sort()
+
+for i in result:
+    print i
